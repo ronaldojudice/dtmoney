d4caa9bb93f2ba6292fab4dfd6067036b94b2850
ronaldojudice <ronaldojudice@live.com>
2021-08-17 23:16:00 -0300

    style configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 
+import { GlobalStyled } from "./styles/global";
+
+
+
 export function App() {
   return (
     <div className="App">
       <h1>Fala povo doido</h1>
+      <GlobalStyled/>
       
     </div>
   );
