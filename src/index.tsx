3a42f95854640a79b9db35e2992560e633ca4baf
ronaldojudice <ronaldojudice@live.com>
2021-08-19 14:34:15 -0300

    axios ok

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,27 @@
 import ReactDOM from 'react-dom';
 import {App} from './App';
+import {createServer} from 'miragejs';
+
+createServer({
+  routes(){
+    this.namespace='api';
+    this.get('/transactions',()=>{
+      return[
+        {
+          id:1,
+          title:'Transaction 1',
+          amount:400,
+          type:'deposit',
+          category:'Food',
+          createAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 
-ReactDOM.render(
-  
-    <App />,
- 
+ReactDOM.render(  
+    <App />, 
   document.getElementById('root')
 );
