14e65af88ce417b7f7390bd1123213825a2cc2d3
ronaldojudice <ronaldojudice@live.com>
2021-08-24 14:47:32 -0300

    MODAL & FORMS Concluido

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,23 @@
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { Container } from "./style";
 import {api} from '../../services/api';
 
+interface Transaction{
+    id:number;
+    title:string;
+    value:number;
+    category:string;
+    type:string;
+    createdAt: string;
+    
+}
+
 export function TransactionsTable(){
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
     useEffect(()=>{
         api.get('transactions')     
-        .then(response => console.log(response.data))
+        .then(response =>setTransactions(response.data.transactions))
     },[]);
     
     return (
@@ -20,18 +32,26 @@ export function TransactionsTable(){
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento de App</td>
-                        <td className="deposit">12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>lanche</td>
-                        <td className="withdraw">2.000</td>
-                        <td>COmida</td>
-                        <td>20/02/2021</td>
-                    </tr>
+                  {transactions.map(transaction=> {
+                      return(
+                          <tr key={transaction.id}>
+                              <td>{transaction.title}</td>
+                              <td className={transaction.type}>
+                                 {Intl.NumberFormat('pt-br',{
+                                     style:'currency',
+                                     currency:'BRL'
+                                 }).format(transaction.value)}
+                                 </td>
+                              <td>{transaction.category}</td>
+                              <td>
+                                 {new Intl.DateTimeFormat('pt-BR').format(
+                                     new Date(transaction.createdAt)
+                                    )}
+                             </td>
+
+                          </tr>
+                      );
+                  })}
                    
                 </tbody>
             </table>
--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,30 @@ createServer({
   models:{
    transactions: Model,
   },
+
+  seeds(server){
+    server.db.loadData({
+      transactions:[
+        {
+          id:1,
+          title:'Frelance Desenvolvimento Web',
+          value:600,
+          category:'Dev',
+          type: 'deposit',
+          createdAt: new Date('2021-08-24 11:15:21'),
+        },
+        {
+          id:2,
+          title:'Lanche',
+          value:60,
+          category:'Comida',
+          type: 'withdraw',
+          createdAt: new Date('2021-08-23 11:15:31'),
+        },
+      ]
+    })
+  },
+
   routes(){
     this.namespace= 'api';
     
